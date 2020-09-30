d19f4c140e5fc2308e26bcc8b93f9a021bb8a670
GPeralta <43755568+gperalta102@users.noreply.github.com>
2020-09-29 23:19:36 -0700

    ~

--- src/com/company/cs3560/assignment01/Questions.java
+++ src/com/company/cs3560/assignment01/Questions.java
@@ -1,4 +1,5 @@
 package com.company.cs3560.assignment01;
 
 public class Questions {
+    
 }
--- src/com/company/cs3560/assignment01/Student.java
+++ src/com/company/cs3560/assignment01/Student.java
@@ -2,5 +2,5 @@ package com.company.cs3560.assignment01;
 
 public class Student {
     private String name;
-    
+
 }
